fb6de625cc2b725d4e5928c66e561c1d0d9307c8
aarostami <alireza.ro76@gmail.com>
2024-09-09 19:22:56 +0330

    first

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
 	images: {
 		unoptimized: true
 	},
+	experimental: {
+		ppr: true
+	},
 	typescript: {
 		// !! WARN !!
 		// Dangerously allow production builds to successfully complete even if
